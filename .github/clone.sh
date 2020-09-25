80e9dd2ec3cdf66df3882e6dc90fe264352c1597
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 02:06:39 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -1,4 +1,11 @@
-git clone -b main https://github.com/sandy1709/catuserbot catt
-mv catt/.git .git
-rm -rf catt
+FILE= .git
+if [ -d "$FILE" ]
+then
+    echo "$FILE directory exists already."
+else
+  git clone -b main https://github.com/sandy1709/catuserbot catt
+  mv catt/.git .git
+  rm -rf catt
+fi
+
 python -m userbot
