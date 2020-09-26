c9ea433d2fdecaffb9a067d4604a4fbf11702afa
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 14:52:31 +0530

    Update herokugiturl.py

--- .github/herokugiturl.py
+++ .github/herokugiturl.py
@@ -1,7 +1,7 @@
 import heroku3
-
+from ..userbot.Config import Config
 try:
-    if Var.HEROKU_APP_NAME not in heroku3.from_key(Var.HEROKU_API_KEY).apps():
-        raise Exception(f"Invalid HEROKU_APP_NAME  {Var.HEROKU_APP_NAME}")
+    if Config.HEROKU_APP_NAME not in heroku3.from_key(Config.HEROKU_API_KEY).apps():
+        raise Exception(f"Invalid HEROKU_APP_NAME  {Config.HEROKU_APP_NAME}")
 except Exception as e:
     print(str(e))
