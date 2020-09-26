9b18f665723a9b32fd75e1af07df72a9b0e941e7
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 18:58:41 +0530

    Update update.py

--- .github/update.py
+++ .github/update.py
@@ -1,24 +1,22 @@
 import asyncio
 import sys
-from os import environ, execle, path, remove
+from os import path
 
 requirements_path = path.join(
     path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
 )
 
-
-def update_requirements():
+async def update_requirements():
     reqs = str(requirements_path)
     try:
-        process = asyncio.create_subprocess_shell(
+        process = await asyncio.create_subprocess_shell(
             " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
-        process.communicate()
+        await process.communicate()
         return process.returncode
     except Exception as e:
         return repr(e)
-
 
 update_requirements()
