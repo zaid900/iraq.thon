85d797c5c59ccca372b4097d69324d1f3a7b6b33
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 13:11:47 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -25,7 +25,7 @@ echo "
 "
 
 if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
-    local herokuErr=$(python ./.github/herokugiturl.py)
+    herokuErr=$(python ./.github/herokugiturl.py)
     [[ $herokuErr ]] && quit "heroku response > $herokuErr"
     declare -g HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
 fi
@@ -35,8 +35,8 @@ FILE=/app/.git
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    if [ -z $HEROKU_GIT_URL ]; then
-        gitClone $HEROKU_GIT_URL cat_ub || quit "Invalid HEROKU_API_KEY or HEROKU_APP_NAME var !"
+    if [ -z "$HEROKU_GIT_URL" ]; then
+        gitClone "$HEROKU_GIT_URL" cat_ub || quit "Invalid HEROKU_API_KEY or HEROKU_APP_NAME var !"
         mv cat_ub/.git .
         rm -rf cat_ub
     else
