0762aa629882da9f54597ddf7d229bca009738ee
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 13:08:34 +0530

    Create herokugiturl.py

---
+++ .github/herokugiturl.py
@@ -0,0 +1,6 @@
+import heroku3
+try:
+    if "'$HEROKU_APP_NAME'" not in heroku3.from_key("'$HEROKU_API_KEY'").apps():
+        raise Exception("Invalid HEROKU_APP_NAME \"'$HEROKU_APP_NAME'\"")
+except Exception as e:
+    print(str(e))
