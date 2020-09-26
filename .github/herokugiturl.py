fd2fdd7651eed107a3e911618c7303ebfef4b288
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 14:56:15 +0530

    Update herokugiturl.py

--- .github/herokugiturl.py
+++ .github/herokugiturl.py
@@ -1,6 +1,6 @@
 import heroku3
 
-from ..userbot.Config import Config
+from userbot.Config import Config
 
 try:
     if Config.HEROKU_APP_NAME not in heroku3.from_key(Config.HEROKU_API_KEY).apps():
