b71a32b305e45640e2e5ab5146648927460f620c
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-09-26 13:09:27 +0530

    Update clone.sh

--- .github/clone.sh
+++ .github/clone.sh
@@ -25,13 +25,7 @@ echo "
 "
 
 if [[ -n $HEROKU_API_KEY && -n $HEROKU_APP_NAME ]]; then
-    local herokuErr=$(runPythonCode '
-import heroku3
-try:
-    if "'$HEROKU_APP_NAME'" not in heroku3.from_key("'$HEROKU_API_KEY'").apps():
-        raise Exception("Invalid HEROKU_APP_NAME \"'$HEROKU_APP_NAME'\"")
-except Exception as e:
-    print(str(e))')
+    local herokuErr=$(python ./.github/herokugiturl.py)
     [[ $herokuErr ]] && quit "heroku response > $herokuErr"
     declare -g HEROKU_GIT_URL="https://api:$HEROKU_API_KEY@git.heroku.com/$HEROKU_APP_NAME.git"
 fi
@@ -41,7 +35,7 @@ FILE=/app/.git
 if [ -d "$FILE" ] ; then
     echo "$FILE directory exists already."
 else
-    if [ -z $HEROKU_GIT_URL]; then
+    if [ -z $HEROKU_GIT_URL ]; then
         gitClone $HEROKU_GIT_URL cat_ub || quit "Invalid HEROKU_API_KEY or HEROKU_APP_NAME var !"
         mv cat_ub/.git .
         rm -rf cat_ub
