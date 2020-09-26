87c5f54de63a028f34ffdeef61b20f7af331ea12
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-26 09:23:25 +0000

    pylint: auto fixes

--- .github/herokugiturl.py
+++ .github/herokugiturl.py
@@ -1,5 +1,7 @@
 import heroku3
+
 from ..userbot.Config import Config
+
 try:
     if Config.HEROKU_APP_NAME not in heroku3.from_key(Config.HEROKU_API_KEY).apps():
         raise Exception(f"Invalid HEROKU_APP_NAME  {Config.HEROKU_APP_NAME}")
