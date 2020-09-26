70f07612d5f17678e1668b6e73a0ba0d3993d026
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-09-26 16:15:15 +0000

    pylint: auto fixes

--- .github/update.py
+++ .github/update.py
@@ -1,18 +1,21 @@
-from userbot import runcmd
 import asyncio
+
+from userbot import runcmd
+
 
 async def update_requirements():
     try:
         await runcmd("pip install --upgrade pip")
         print("Pip is upto-date")
     except BaseException:
-        print("Error while updating pip")          
+        print("Error while updating pip")
     try:
         await runcmd(catcmd)
         print("Succesfully requiremenst are upto-date")
     except BaseException:
         print("Error while installing requirments")
-        
+
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(update_requirements())
-loop.close()        
+loop.close()
